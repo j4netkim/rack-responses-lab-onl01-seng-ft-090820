a7b411334a1b96f9922108f4b8285a45255390c3
J K <jkim65@buffalo.edu>
2020-10-19 22:34:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,7 @@ class Application
 
     if t1 > 12 || t1 == 12
       resp.write "Good Afternoon!"
-    elsif t1 != 12 && t1 < 12
-      resp.write "Good Morning!"
-    else
+    elsif t1 != 12 || t1 !> 12
       resp.write "Good Morning!"
     end
     resp.finish
